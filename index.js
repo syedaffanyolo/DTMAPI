60573c9514c3fabd00611ca4e455809f3c2374df
Syed Affan <Syedaffan070@gmail.com>
2022-01-23 13:28:41 +0530

    NEW API

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ const importDatagr = require("./grapi.json");
 const importDatan = require("./napi.json");
 const importDatad = require("./dapi.json");
 const importDataDetailGr = require("./detailgr.json");
+const importDataDetailGr1 = require("./detailgr1.json");
+const importDataDetailGr2 = require("./detailgr2.json");
+const importDataDetailGr3 = require("./detailgr3.json");
 const importDataDetailN = require("./detailn.json");
 const importDataDetailD = require("./detaild.json");
 const importDataShopN = require("./shopn.json");
 let port = process.env.PORT || 3000;
-
+//lala
 app.get("/", function(req,res){
     res.send("hello");
 });
 
 app.get("/detailgr", function(req,res){
     res.send(importDataDetailGr);
+});
+app.get("/detailgr1", function(req,res){
+    res.send(importDataDetailGr1);
+});
+app.get("/detailgr2", function(req,res){
+    res.send(importDataDetailGr2);
+});
+app.get("/detailgr3", function(req,res){
+    res.send(importDataDetailGr3);
 });
 
 app.get("/detailn", function(req,res){
